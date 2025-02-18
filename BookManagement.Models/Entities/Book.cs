47b526f7d546812254600b4e3d88af497ceeba56
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-02-18 12:01:03 +0100

    Added models: Book and User

---
+++ BookManagement.Models/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookManagement.Models.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public int PublicationYear { get; set; }
+        public int ViewsCount { get; set; } = 0;
+    }
+}
