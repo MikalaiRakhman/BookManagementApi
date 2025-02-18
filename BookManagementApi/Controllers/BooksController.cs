043b8a8a13acedf70b9aa329efba64170bcf9766
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-02-18 16:20:58 +0100

    Created BooksController

---
+++ BookManagementApi/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using BookManagement.DataAccess.Operations.BooksOperations.CreateBook;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BooksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("create-book")]
+        public async Task<IActionResult> CreateBook([FromBody] CreateBookCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+    }
+}
