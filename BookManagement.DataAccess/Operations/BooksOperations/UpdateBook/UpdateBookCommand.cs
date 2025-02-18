516e2c3e9ea053d0482837feaa705c2b0c1cc25c
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-02-18 20:49:05 +0100

    Added UpdateBookCommand

---
+++ BookManagement.DataAccess/Operations/BooksOperations/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BookManagement.DataAccess.Operations.BooksOperations.UpdateBook
+{
+    public class UpdateBookCommand: IRequest
+    {
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
