29c6325306cc408fc3e05cabbdf34cec875fd8f0
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-02-19 19:34:06 +0100

    Rule for CreateBookCommand ready.

---
+++ BookManagement.DataAccess/Operations/BooksOperations/AddBooks/AddBooksCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using BookManagement.Models.Entities;
+using FluentValidation;
+
+namespace BookManagement.DataAccess.Operations.BooksOperations.AddBooks
+{
+    public class AddBooksCommandValidator: AbstractValidator<AddBooksCommand>
+    {
+        public AddBooksCommandValidator()
+        {
+            RuleFor(b => b.Books)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Books cannot be null or empty");
+                .SetValidator(new BookValidator());
+
+
+        }
+    }
+}
