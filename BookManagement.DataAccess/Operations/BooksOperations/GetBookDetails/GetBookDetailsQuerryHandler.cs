1bf2b652e44ade5be5c85bc0f9f55a0662fa13f5
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-02-19 15:31:52 +0100

    Created GetBookDetails

---
+++ BookManagement.DataAccess/Operations/BooksOperations/GetBookDetails/GetBookDetailsQuerryHandler.cs
@@ -0,0 +1,22 @@
+﻿using BookManagement.DataAccess.Data;
+using BookManagement.Models.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.DataAccess.Operations.BooksOperations.GetBookDetails
+{
+    public class GetBookDetailsQuerryHandler: IRequestHandler<GetBookDetailsQuerry, Book>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetBookDetailsQuerryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> Handle(GetBookDetailsQuerry request, CancellationToken cancellationToken)
+        {
+            return await _context.Books.FirstAsync(x => x.Title == request.Title);
+        }
+    }
+}
